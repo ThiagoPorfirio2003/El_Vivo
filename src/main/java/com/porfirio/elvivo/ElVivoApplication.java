64732d38ebb9c554b87f6878eb3f93fb8decd1ef
Thiago Lisandro Porfirio <thiagolisandroporfirio@gmail.com>
2025-05-12 16:06:28 -0300

    1er commit

---
+++ src/main/java/com/porfirio/elvivo/ElVivoApplication.java
@@ -0,0 +1,13 @@
+package com.porfirio.elvivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElVivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElVivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porfirio/elvivo/ElVivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfirio.elvivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElVivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
